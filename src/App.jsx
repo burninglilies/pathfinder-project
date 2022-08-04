0baccdd9cfb26f8f2594c3cba985065037b23987
Lily Pham <lnp35@cornell.edu>
2022-08-04 19:21:11 -0400

    added current files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Pathfinder from './components/pathfinder/Pathfinder'
+import PathfinderUI from './components/pathfinder/PathfinderUI'
+
 
 const App = () => {
     return (
         <div className="App">
-            <Pathfinder/>
+            <PathfinderUI/>
         </div>
     )
 }
